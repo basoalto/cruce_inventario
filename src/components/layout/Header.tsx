7928487a77144ac51ed21cd8722410e4e0e1d790
Lucas <lucasbasoaltocontreras@gmail.com>
2024-10-21 16:14:17 -0300

    tipar toggleDarkMode

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,7 +1,13 @@
 // src/components/Header.tsx
+import React from "react";
 import Navbar from "./NavBar";
 
-const Header = ({ toggleDarkMode }) => {
+// Define the type for the toggleDarkMode prop
+interface HeaderProps {
+  toggleDarkMode: () => void; // You can specify the function signature if necessary
+}
+
+const Header: React.FC<HeaderProps> = ({ toggleDarkMode }) => {
   return (
     <header>
       <Navbar toggleDarkMode={toggleDarkMode} />
