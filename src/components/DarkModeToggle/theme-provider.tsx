e1803850a44504bab170fde7587f35531aa1bbe3
Lucas <lucasbasoaltocontreras@gmail.com>
2024-10-23 16:15:39 -0300

    dejar por defecto en modo claro

--- src/components/DarkModeToggle/theme-provider.tsx
+++ src/components/DarkModeToggle/theme-provider.tsx
@@ -1,9 +1,17 @@
-"use client"
+"use client";
 
-import * as React from "react"
-import { ThemeProvider as NextThemesProvider } from "next-themes"
-import { type ThemeProviderProps } from "next-themes/dist/types"
+import * as React from "react";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
+import { type ThemeProviderProps } from "next-themes/dist/types";
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
+  return (
+    <NextThemesProvider
+      enableSystem={true} // Habilita el uso del tema del sistema
+      defaultTheme="light" // Define el tema claro por defecto
+      {...props}
+    >
+      {children}
+    </NextThemesProvider>
+  );
 }
