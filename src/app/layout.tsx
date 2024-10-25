703e1e2cf6c05ae155404b2cffdda1ae80a80d3c
Lucas <lucasbasoaltocontreras@gmail.com>
2024-10-25 09:10:33 -0300

    agregar imagen a favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/logo negro.png" type="image/png" />
+        <link rel="icon" href="/logo-negro.png" type="image/png" />
         <title>Cruce Inventario</title>
       </head>
       <body className={inter.className}>
